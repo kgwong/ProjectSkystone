58da46e6763283beb7493ca1203c69efda980538
Kevin Wong <kgwong1@uci.edu>
2015-12-16 15:55:06 -0800

    Make minor changes

--- Skystone/RenderComponent.h
+++ Skystone/RenderComponent.h
@@ -11,15 +11,6 @@ public:
 
 	virtual Component::Type getType() final;
 
-	int x() { 
-		std::cout << "this is actually getting used" << std::endl;
-		return owner_.getPosX(); 
-	}
-	int y() { 
-		std::cout << "this is actualyl getting used" << std::endl;
-		return owner_.getPosY(); 
-	}
-
 	virtual int getWidth();
 	virtual int getHeight();
 
