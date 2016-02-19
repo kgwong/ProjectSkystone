83f624ec7c246c0e074a9dcbc57f9623a36a62bc
cnavasca59 <christian.navasca@gmail.com>
2016-02-19 09:14:40 -0800

    Don't render text unless it isn't empty

--- Skystone/Components/Render/TextRenderer.cpp
+++ Skystone/Components/Render/TextRenderer.cpp
@@ -19,14 +19,19 @@ TextRenderer::~TextRenderer()
 
 void TextRenderer::render(GameWindow& window, float percentBehind)
 {
-	SDL_Color color = { 255, 255, 255 };
-	SDL_Surface* message = TTF_RenderText_Blended(font, "", color);
-	SDL_Texture* text_texture = SDL_CreateTextureFromSurface(window.getRenderer(), message);
+	std::string text = "";
 
-	int w, h;
-	SDL_QueryTexture(text_texture, NULL, NULL, &w, &h);
+	if (text != "")
+	{
+		SDL_Color color = { 255, 255, 255 };
+		SDL_Surface* message = TTF_RenderText_Blended(font, text.c_str(), color);
+		SDL_Texture* text_texture = SDL_CreateTextureFromSurface(window.getRenderer(), message);
 
-	SDL_Rect src = { 0,0, w, h};
-	SDL_Rect dest = { 0, 0, w, h};
-	window.render(text_texture, &src, &dest);
+		int w, h;
+		SDL_QueryTexture(text_texture, NULL, NULL, &w, &h);
+
+		SDL_Rect src = { 0,0, w, h };
+		SDL_Rect dest = { 0, 0, w, h };
+		window.render(text_texture, &src, &dest);
+	}
 }
