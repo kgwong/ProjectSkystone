0e5fc40ea803b4d0ebe56f75c7bcf3873e7fab1a
cnavasca59 <christian.navasca@gmail.com>
2016-04-08 16:51:21 -0700

    Change size of item on main menu instead of highlight

--- Skystone/Components/Render/TextRenderer.cpp
+++ Skystone/Components/Render/TextRenderer.cpp
@@ -4,8 +4,11 @@
 #include "Application/Log.h"
 #include <SDL/SDL.h>
 
+const static int defaultFontSize = 28;
+const static std::string defaultFontPath = "../Assets/Fonts/LDFComicSans.ttf";
+
 TextRenderer::TextRenderer(GameObject& owner)
-	: RenderComponent(owner), font_(TTF_OpenFont("../Assets/Fonts/LDFComicSans.ttf", 28)), text_("")
+	: RenderComponent(owner), font_(TTF_OpenFont(defaultFontPath.c_str(), defaultFontSize)), text_("")
 {
 	if (font_ == nullptr)
 		LOG("Warning") << "No font loaded: " << TTF_GetError();
@@ -21,14 +24,14 @@ void TextRenderer::render(GameWindow& window, float percentBehind)
 	if (text_ != "")
 	{
 		SDL_Color color = { 255, 255, 255 };
-		SDL_Surface* message = TTF_RenderText_Shaded(font_, text_.c_str(), color, bg_);
+		SDL_Surface* message = TTF_RenderText_Blended(font_, text_.c_str(), color);
 		SDL_Texture* text_texture = SDL_CreateTextureFromSurface(window.getRenderer(), message);
 
 		int w, h;
 		SDL_QueryTexture(text_texture, NULL, NULL, &w, &h);
 
 		SDL_Rect src = { 0,0, w, h };
-		SDL_Rect dest = { (int)owner_.getPosX(), (int)owner_.getPosY(), w, h };
+		SDL_Rect dest = { (int)owner_.getPosX()-(w/2), (int)owner_.getPosY()-(h/2), w, h };
 		window.render(text_texture, &src, &dest);
 		SDL_FreeSurface(message);
 		SDL_DestroyTexture(text_texture);
@@ -45,7 +48,8 @@ void TextRenderer::setText(const std::string& newText)
 	text_ = newText;
 }
 
-void TextRenderer::setBg(SDL_Color bg)
+void TextRenderer::setFontSize(int size)
 {
-	bg_ = bg;
+	TTF_CloseFont(font_);
+	font_ = TTF_OpenFont(defaultFontPath.c_str(), size);
 }
--- Skystone/Components/Render/TextRenderer.h
+++ Skystone/Components/Render/TextRenderer.h
@@ -15,12 +15,11 @@ public:
 
 	std::string getText();
 	void setText(const std::string& newText);
-	void setBg(SDL_Color bg);
+	void setFontSize(int size);
 
 private:
 	TTF_Font* font_;
 	std::string text_;
-	SDL_Color bg_;
 };
 
 #endif //TEXT_RENDERER_H
